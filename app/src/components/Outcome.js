fe3746e5ff42465989cd4a87d952675834c26975
Stephen Algeo <algeos@yw.co.uk>
2020-02-16 19:20:38 +0000

    Add toggle for odds type

--- app/src/components/Outcome.js
+++ app/src/components/Outcome.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import useOutcome from "../hooks/useOutcome";
+import { OddsContext } from "../App";
 
 const Outcome = ({ id }) => {
+  const [oddsType] = useContext(OddsContext);
   const outcome = useOutcome(id);
 
   if (!outcome) return <div>Loading...</div>;
 
   const {
     name,
-    price: { num, den }
+    price: { num, den, decimal }
   } = outcome;
 
   return (
     <div>
-      {name} {num}/{den}
+      {name} {oddsType === "fractional" ? `${num}/${den}` : decimal}
     </div>
   );
 };
