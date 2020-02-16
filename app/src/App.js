7efef2860f8389821d1692f0ec16497826d07b7d
Stephen Algeo <algeos@yw.co.uk>
2020-02-16 19:20:38 +0000

    Add toggle for odds type

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Live from "./Live";
 
+export const OddsContext = React.createContext();
+
 function App() {
+  const reducer = state => (state === "fractional" ? "decimal" : "fractional");
+  const [oddsType, toggle] = useReducer(reducer, "fractional");
+
   return (
-    <div className="App">
-      <header className="App-header">Stars Tech Test</header>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Live />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <OddsContext.Provider value={[oddsType, toggle]}>
+      <div className="App">
+        <header className="App-header">Stars Tech Test</header>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Live />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </OddsContext.Provider>
   );
 }
 
--- app/src/Live.js
+++ app/src/Live.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Market from "./components/Market";
 import useLiveEvents from "./hooks/useLiveEvents";
+import { OddsContext } from "./App";
 
 const Live = () => {
+  const [oddsType, toggle] = useContext(OddsContext);
   const events = useLiveEvents();
 
   return events.length === 0 ? (
     <div>Loading...</div>
   ) : (
     <React.Fragment>
+      <button onClick={toggle}>Odds: {oddsType}</button>
       {events.map(({ eventId, name, markets }) => (
         <div key={eventId} className="c-event">
           <h2 key={eventId}>{name}</h2>
